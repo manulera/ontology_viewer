6c5599077a51c90bd236080d703008d19e68146b
manulera <manulera14@gmail.com>
2022-05-03 18:58:42 +0100

    reorganised to serve with flask and fixed obo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request, current_app
 from flask_cors import CORS
 import pronto
 import requests
@@ -7,25 +7,36 @@ app = Flask(__name__)
 CORS(app)
 app.config["CACHE_TYPE"] = "null"
 
-@app.route('/', methods = ['POST','GET'])
-def main():
+@app.route('/', methods = ['GET'])
+def index():
+    return current_app.send_static_file('index.html')
+
+
+@app.route('/request', methods = ['POST'])
+def main_request():
     data = request.get_json()
 
-    fypo = pronto.Ontology("https://github.com/geneontology/go-ontology/raw/master/src/ontology/go-edit.obo")
+    fypo = pronto.Ontology("http://current.geneontology.org/ontology/go.obo")
 
-    starting_point = fypo[data['id']].subclasses().to_set()
+    children = fypo[data['id']].subclasses().to_set()
 
     string_body = '```mermaid\ngraph LR;\n'
 
     def editName(name):
         return "<br>".join(name[i:i+20] for i in range(0, len(name), 20))
 
-    for term in starting_point:
-        print(term)
-        parents = term.superclasses(with_self=False, distance=1).to_set() & starting_point
-        for parent in parents:
-            string_body+=f'      {parent.id}[{parent.id}<br>{editName(parent.name)}]-->{term.id}[{term.id}<br>{editName(term.name)}];\n'
+    for child in children:
+        # Parents of this child that are linked to the selected term (that's why it does the intersection with the children)
+        parent_subset = child.superclasses(with_self=False, distance=1).to_set() & children
+        for parent in parent_subset:
+            string_body+=f'      {parent.id}[{parent.id}<br>{editName(parent.name)}]-->{child.id}[{child.id}<br>{editName(child.name)}];\n'
 
+    parents = fypo[data['id']].superclasses().to_set()
+    for parent in parents:
+        # Children of this parent that are linked to the selected term (that's why it does the intersection)
+        child_subset = parent.subclasses(with_self=False, distance=1).to_set() & parents
+        for child in child_subset:
+            string_body+=f'      {parent.id}[{parent.id}<br>{editName(parent.name)}]-->{child.id}[{child.id}<br>{editName(child.name)}];\n'
 
     string_body+='```\n'
 
